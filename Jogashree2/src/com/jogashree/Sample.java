8d2524d091321e16bf3968e0afd4338cba843f58
Jhilly <Jhilly@LAPTOP-EELKOJ1U.fios-router.home>
2021-01-03 22:49:37 -0500

    Adding new project

---
+++ Jogashree2/src/com/jogashree/Sample.java
@@ -0,0 +1,17 @@
+package com.jogashree;
+
+public class Sample {
+	int a=10;
+	void m1() {
+		System.out.println(a);
+		System.out.println("Welcome to java class");
+	  }
+
+	public static void main(String[] args) {
+	Sample s=new Sample();
+	s.m1();
+	System.out.println(s.a);
+
+	}
+
+}
